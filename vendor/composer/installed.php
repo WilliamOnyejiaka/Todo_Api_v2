966cdf017192e19a4afa1d8069f403ddea0ec9e0
William Onyejiaka <williamonyejiaka08062528003@gmail.com>
2022-05-05 02:15:46 +0100

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'user/todo_api_v2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.1.2',
+            'version' => '6.1.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => 'c297139da7c6873dbd67cbd1093f09ec0bbd0c50',
+            'dev_requirement' => false,
+        ),
+        'user/todo_api_v2' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'wonder-tech/wonder-ral' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wonder-tech/wonder-ral',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '71baab04cc76b8a05c44bc56cd98fb5ceccb2a1c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
