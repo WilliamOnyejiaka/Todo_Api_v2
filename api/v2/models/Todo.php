c2999c01b99ed223345ab2a31dc57226a146f4a7
William Onyejiaka <williamonyejiaka08062528003@gmail.com>
2022-05-07 06:52:19 +0100

    Finished the todo routes

--- api/v2/models/Todo.php
+++ api/v2/models/Todo.php
@@ -31,5 +31,36 @@ class Todo {
     $stmt->execute();
     return $stmt->get_result();
   }
+
+  public function upate_timestamp(int $id){
+    $query = "UPDATE $this->tbl_name SET updated_at = now() WHERE id = ?";
+    $stmt = $this->connection->prepare($query);
+    $stmt->bind_param("i",$id);
+    return $stmt->execute() ? true : false;
+  }
+
+  public function update_title(int $id,int $user_id,string $title){
+    $query = "UPDATE $this->tbl_name SET title = ? WHERE id = ? AND user_id = ? ";
+    $stmt = $this->connection->prepare($query);
+    $title = htmlspecialchars(strip_tags($title));
+    $stmt->bind_param("sii",$title,$id,$user_id);
+    $this->upate_timestamp($id);
+    return $stmt->execute() ? true : false;
+  }
+
+  public function update_completed(int $id,int $user_id,int $completed){
+    $query = "UPDATE $this->tbl_name SET completed = ? WHERE id = ? AND user_id = ? ";
+    $stmt = $this->connection->prepare($query);
+    $stmt->bind_param("iii",$completed,$id,$user_id);
+    $this->upate_timestamp($id);
+    return $stmt->execute() ? true : false;
+  }
+
+  public function delete_todo($id,$user_id){
+    $query = "DELETE FROM $this->tbl_name WHERE id = ? AND user_id = ?";
+    $stmt = $this->connection->prepare($query);
+    $stmt->bind_param("ii",$id,$user_id);
+    return $stmt->execute() ? true : false;
+  }
 }
 ?>
--- api/v2/routes/todo/index.php
+++ api/v2/routes/todo/index.php
@@ -80,4 +80,106 @@ $todo->get("/search",fn() => (new Controller())->access_token_controller(functio
 
 }));
 
+$todo->patch("/update_todo/title",fn() => (new Controller())->access_token_controller(function($payload,$body){
+  (new Validator())->validate_body($body,['id','title']);
+  $response = new Response();
+
+  $todo_id = intval($body->id);
+  $user_id = $payload->data->id;
+  $todo = new Todo((new Database(config('host'),config('username'),config('password'),config('database_name')))->connect());
+  $todo_item = $todo->get_todo($todo_id,$user_id);
+  $todo_exits = (new Serializer(['id']))->tuple($todo_item);
+
+  if($todo_exits){
+    if($todo->update_title($todo_id,$user_id,$body->title)){
+      $response->send_response(200,[['error',false],['message','todo title updated successfully']]);
+    }else{
+      $response->send_response(500,[['error',true],['message','something went wrong']]);
+    }
+  }else {
+    $response->send_response(404,[['error',true],['message','todo not found']]);
+  }
+
+}));
+
+$todo->patch("/update_todo/completed",fn() => (new Controller())->access_token_controller(function($payload,$body){
+  (new Validator())->validate_body($body,['id']);
+  $completed_isset = isset($body->completed);
+  $response = new Response();
+
+  if(!$completed_isset){
+    $response->send_response(400,[['error',true],['message','all values needed']]);
+    exit();
+  }else if($body->completed > 1 || $body->completed < 0){
+    $response->send_response(400,[['error',true],['message','0 or 1 allowed']]);
+  }else {
+    $todo_id = intval($body->id);
+    $user_id = $payload->data->id;
+    $todo = new Todo((new Database(config('host'),config('username'),config('password'),config('database_name')))->connect());
+    $todo_item = $todo->get_todo($todo_id,$user_id);
+    $todo_exits = (new Serializer(['id']))->tuple($todo_item);
+
+    if($todo_exits){
+      if($todo->update_completed($todo_id,$user_id,$body->completed)){
+        $response->send_response(200,[['error',false],['message','todo completed updated successfully']]);
+      }else{
+        $response->send_response(500,[['error',true],['message','something went wrong']]);
+      }
+    }else {
+      $response->send_response(404,[['error',true],['message','todo not found']]);
+    }
+  }
+
+}));
+
+$todo->put("/update_todo",fn() => (new Controller())->access_token_controller(function($payload,$body){
+  (new Validator())->validate_body($body,['id','title']);
+  $completed_isset = isset($body->completed);
+  $response = new Response();
+
+  if(!$completed_isset){
+    $response->send_response(400,[['error',true],['message','all values needed']]);
+    exit();
+  }else if($body->completed > 1 || $body->completed < 0){
+    $response->send_response(400,[['error',true],['message','0 or 1 allowed']]);
+  }else {
+    $todo_id = intval($body->id);
+    $user_id = $payload->data->id;
+    $todo = new Todo((new Database(config('host'),config('username'),config('password'),config('database_name')))->connect());
+    $todo_item = $todo->get_todo($todo_id,$user_id);
+    $todo_exits = (new Serializer(['id']))->tuple($todo_item);
+
+    if($todo_exits){
+      if($todo->update_title($todo_id,$user_id,$body->title) && $todo->update_completed($todo_id,$user_id,$body->completed)){
+        $response->send_response(200,[['error',false],['message','todo updated successfully']]);
+      }else{
+        $response->send_response(500,[['error',true],['message','something went wrong']]);
+      }
+    }else {
+      $response->send_response(404,[['error',true],['message','todo not found']]);
+    }
+  }
+
+}));
+
+$todo->delete("/delete",fn() => (new Controller())->access_token_controller(function($payload,$body){
+  $todo_id = intval((new Validator())->validate_query_strings(['todo_id'])['todo_id']);
+  $user_id = $payload->data->id;
+  $todo = new Todo((new Database(config('host'),config('username'),config('password'),config('database_name')))->connect());
+  $todo_exits = (new Serializer(['id']))->tuple($todo->get_todo($todo_id,$user_id));
+
+  $response = new Response();
+
+  if($todo_exits){
+    if($todo->delete_todo($todo_id,$user_id)){
+      $response->send_response(200,[['error',false],['message','todo deleted successfully']]);
+    }else{
+      $response->send_response(500,[['error',true],['message','something went wrong']]);
+    }
+  }else {
+    $response->send_response(404,[['error',true],['message','todo not found']]);
+  }
+
+}));
+
 $todo->run();
